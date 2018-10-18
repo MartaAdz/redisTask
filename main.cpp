051c16d99a780d2e59ff2fdb997a7e338880fcf4
Marta <adzgausku@gmail.com>
2018-10-18 23:40:22 +0300

    adding user w account

--- main.cpp
+++ main.cpp
@@ -2,6 +2,31 @@
 #include <string>
 #include <hiredis/hiredis.h>
 
+void newUser(){
+    std::cout<<"User name: ";
+    std::string name;
+    std::cin>> name;
+    auto accountNumber=(rand()%999) +100;
+    void *setUser = redisCommand (c, "HMSET user %s accountNumber", name);
+
+    std::cout<<"Credit? y/n";
+    char ifCredit;
+    std::cin>> ifCredit;
+    if (ifCredit=='y'){
+        std::cout<<"set credit limit";
+        std::string creditLimit;
+        std::cin>>creditLimit;
+        void *setAccountLimit = redisCommand (c, "SET accountNumber:credit %s", creditLimit);
+        ifCredit="credit";
+    }
+    else {
+        std::cout<<"you have 20$ in your account";
+        void *setAccountLimit = redisCommand (c, "SET accountNumber:debit 20");
+        ifCredit="debit";
+    }
+
+}
+
 int main() {
     redisContext *c = redisConnect("127.0.0.1", 6379);
     if (c == NULL || c->err) {
@@ -13,16 +38,15 @@ int main() {
         }
     }
 
-    std::cout<<"User name: ";
-    std::string name;
-    std::cin>> name;
-    void *setUser = redisCommand (c, "HMSET user %s LT100", name);
+    newUser();
+    
+
     if (setUser != NULL) {
         std::cout<<"user is in database"<<std::endl;
         freeReplyObject(setUser);
     }
     else {
-        std::cout<<"TUser is NOT in database"<<c->err <<std::endl;
+        std::cout<<"User is NOT in database"<<c->err <<std::endl;
     }
     
 
